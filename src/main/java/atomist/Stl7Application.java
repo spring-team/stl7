390c646a7e79c2d7f99b28c51ddca844e81dae51
Rod Johnson <johnsonroda@gmail.com>
2018-03-01 16:47:37 -0600

    Initial commit from Atomist

---
+++ src/main/java/atomist/Stl7Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stl7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stl7Application.class, args);
+	}
+}
